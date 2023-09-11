b130ca2712ba89bf128772d94df045eeae7ee3b5
queen <Queenmoncler@gmail.com>
2023-09-11 18:54:26 +0300

    bot connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String botName = "ChatBotGamer_bot";
+        String botToken = "6120623714:AAExsw4wmpbeVpj2QCA7adFAWV1b8yxWaBs";
+        TelegramBotsApi telegramBotsApi = null;
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new EchoJavaTelegramBot(botName, botToken));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/org/example/MyBot.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public String getBotUsername() {
+        // геттер имени бота
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        // геттер токена бота
+        return null;
+    }
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // Проверяем появление нового сообщения в чате, и если это текст
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message_text = update.getMessage().getText();            // Создаем переменную равную тексту присланного сообщения
+            String chat_id = update.getMessage().getChatId().toString();    // Создаем переменную равную ид чата присланного сообщения
+
+            SendMessage message = new SendMessage(); // Создаем обект-сообщение
+            message.setChatId(chat_id);              // Передаем чат ид
+            message.setText(message_text);           // Передаем эхо сообщение
+
+            try {
+                execute(message);                   // Отправляем обект-сообщение пользователю
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
